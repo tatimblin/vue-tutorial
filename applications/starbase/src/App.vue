a9abc85104f5c7a08ef97c79666e1fd3f1604753
Tristan Timblin <tatimblin@gmail.com>
2018-03-28 16:10:02 -0400

    Parts 1 and 2 of 5

---
+++ applications/starbase/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="app">
+        <h3>{{title}}</h3>
+        <div class="col-md-12">
+            <Character 
+                v-bind:id="id"
+                v-for="(id, index) in initial_ids"
+                key="index"
+            />
+        </div>
+    </div>
+</template>
+
+
+<script>
+import Character from './components/Character.vue'
+    
+export default {
+    name: 'app',
+    data() {
+        return {
+            title: 'Generate your team',
+            initial_ids: [1, 13, 14]
+        }
+    },
+    components: {
+        Character
+    }
+}
+</script>
