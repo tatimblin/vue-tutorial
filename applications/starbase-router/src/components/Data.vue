3d0342a06ea3c308facf392980dec129505aaa69
Tristan Timblin <tatimblin@gmail.com>
2018-03-28 17:25:02 -0400

    Started vue-router app.

---
+++ applications/starbase-router/src/components/Data.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+    {{items}}
+</div>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            type: this.$route.params.type,
+            items: []
+        }
+    },
+    watch: {
+        '$route': 'fetchItems'
+    },
+    methods: {
+        fetchItems() {
+            this.items = []
+            this.type = this.$route.params.type
+            let initial_ids = [1, 13, 14]
+            
+            for (let i in initial_ids) {
+                let id = initial_ids[i]
+                console.log('id', id)
+                fetch(`https://swapi.co/api/${this.type}/${id}`, {
+                     method: 'GET'
+                })
+                    .then(response => response.json())
+                    .then(json => this.items.push(json))
+            }
+        }
+    },
+    created() {
+        this.fetchItems()
+    }
+}
+</script>
--- applications/starbase/src/main.js
+++ applications/starbase/src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-new Vue({
+new Vue ({
     el: '#app',
-    render: h =>h(App)
+    render: h => h(App)
 })
+
